0345a9390b37571525f6f4434be0199536761073
Antonio Messina <antonio.s.messina@gmail.com>
2012-09-27 22:19:44 +0000

    Add solution to exercise C, part 3

---
+++ ex3c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def maxarg(**kw):
+    """
+    Write a maxarg function, that takes
+    arbitrary keyword arguments (but assume the values
+    are all numbers), and returns the name of the
+    argument with the largest value.
+    """
+    maxval = max(kw.values())
+    for key in kw:
+        if kw[key] == maxval:
+            return key
+
+
+if __name__ == "__main__":
+    assert maxarg(a=1, b=2, c=-1) == 'b'
