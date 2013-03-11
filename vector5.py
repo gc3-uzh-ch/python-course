57b1b991dfbd6ee3c7e00fd0c71f473d92af2317
Antonio Messina <antonio.s.messina@gmail.com>
2013-03-11 20:23:56 +0000

    Add __mul__ special method to Vector
    	modified:   part07.pdf
    	modified:   part07.tex
    	new file:   vector5.py

---
+++ vector5.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+class Vector(object):
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def __mul__(self, scalar):
+    return Vector(scalar*self.x, scalar*self.y)
+  def __str__(self):
+    return ("<%g,%g>" % (self.x, self.y))
+  def __eq__(self, other):
+    return (self.x == other.x) and (self.y == other.y)
+  def __add__(self, other):
+    return Vector(self.x+other.x, self.y+other.y)
+
+
+if __name__ == '__main__':
+    v1 = Vector(0,1)
+    assert v1 * 3 == Vector(0,3)
