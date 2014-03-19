5111d7b1fe8771c9a109f3dfdc3e3724c47651b5
Antonio Messina <antonio.s.messina@gmail.com>
2014-03-19 22:31:38 +0100

    Rename exercise files *before* splitting part08 in two

--- grep.py
+++ grep.py
@@ -8,47 +8,18 @@ class Grep(object):
         self._file = open(filename, 'r')
         self._pattern = pattern
 
-    def match(self, line):
-        return re.search(self._pattern, line)
-
-    def result(self, match, line):
-        return line
+    def __iter__(self):
+        return self
 
     def next(self):
         line = self._file.next()
-        match = self.match(line)
-        while not match:
+        while not re.search(self._pattern, line):
             line = self._file.next()
-            match = self.match(line)
-        return self.result(match, line)
-
-
-class GrepOnlyMatching(Grep):
-
-    def result(self, match, line):
-        return match.group(0)
-
-
-class GrepExactly(Grep):
-
-    def match(self, line):
-        return (self._pattern in line)
+        return line
 
 
 if __name__ == '__main__':
     # test Grep
     grep = Grep(__file__, 'class Grep[A-Za-z]+')
-    assert 'class GrepOnlyMatching(Grep):\n' == grep.next()
-    assert 'class GrepExactly(Grep):\n' == grep.next()
-
-
-    # test GrepOnlyMatching
-    grep = GrepOnlyMatching(__file__, 'class Grep[A-Za-z]+')
-    assert 'class GrepOnlyMatching' == grep.next()
-    assert 'class GrepExactly' == grep.next()
-
-    # test GrepExactly
-    grep = GrepExactly(__file__, 'class Grep')
-    assert 'class Grep(object):\n' == grep.next()
-    assert 'class GrepOnlyMatching(Grep):\n' == grep.next()
-    assert 'class GrepExactly(Grep):\n' == grep.next()
+    assert 'class GrepOnlyMatching(object):\n' == grep.next()
+    assert 'class GrepExactly(object):\n' == grep.next()
--- grep2.py
+++ grep2.py
@@ -1,32 +1,54 @@
+#!/usr/bin/env python
+
 import re
 
 class Grep(object):
+
     def __init__(self, filename, pattern):
-        self._stream = open(filename, 'r')
+        self._file = open(filename, 'r')
         self._pattern = pattern
 
-    def next(self):
-        for line in self._stream:
-            if re.search(self._pattern, line):
-                return line
+    def match(self, line):
+        return re.search(self._pattern, line)
 
-class GrepOnlyMatching(object):
-    def __init__(self, filename, pattern):
-        self._stream = open(filename, 'r')
-        self._pattern = pattern
+    def result(self, match, line):
+        return line
 
     def next(self):
-        for line in self._stream:
-            m = re.search(self._pattern, line)
-            if m is not None:
-                return m.group(0)
+        line = self._file.next()
+        match = self.match(line)
+        while not match:
+            line = self._file.next()
+            match = self.match(line)
+        return self.result(match, line)
 
-class GrepExactly(object):
-    def __init__(self, filename, pattern):
-        self._stream = open(filename, 'r')
-        self._pattern = pattern
 
-    def next(self):
-        for line in self._stream:
-            if self._pattern in line:
-                return line
+class GrepOnlyMatching(Grep):
+
+    def result(self, match, line):
+        return match.group(0)
+
+
+class GrepExactly(Grep):
+
+    def match(self, line):
+        return (self._pattern in line)
+
+
+if __name__ == '__main__':
+    # test Grep
+    grep = Grep(__file__, 'class Grep[A-Za-z]+')
+    assert 'class GrepOnlyMatching(Grep):\n' == grep.next()
+    assert 'class GrepExactly(Grep):\n' == grep.next()
+
+
+    # test GrepOnlyMatching
+    grep = GrepOnlyMatching(__file__, 'class Grep[A-Za-z]+')
+    assert 'class GrepOnlyMatching' == grep.next()
+    assert 'class GrepExactly' == grep.next()
+
+    # test GrepExactly
+    grep = GrepExactly(__file__, 'class Grep')
+    assert 'class Grep(object):\n' == grep.next()
+    assert 'class GrepOnlyMatching(Grep):\n' == grep.next()
+    assert 'class GrepExactly(Grep):\n' == grep.next()
