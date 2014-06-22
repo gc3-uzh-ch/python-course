2f5a2a9f74d0521a321a4bbb797645acb56070ec
Riccardo Murri <riccardo.murri@gmail.com>
2014-06-22 23:19:21 +0200

    Revised part02 for the June 2014 course.

---
+++ ex02d.py
@@ -0,0 +1,12 @@
+def greet(name):
+    """
+    A friendly function.
+    """
+    if name == "Antonio":
+        print ("Welcome Back!")
+    else:
+        print ("Greetings, " + name + "!")
+
+# greet the instructors
+greet("Riccardo")
+greet("Antonio")
